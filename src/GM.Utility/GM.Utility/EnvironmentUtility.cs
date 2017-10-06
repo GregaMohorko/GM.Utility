90866c5bd95ced953fcb8f70ee8687772650af99
Grega Mohorko <grega@mohorko.info>
2017-10-06 22:41:11 +0200

    Added Environment utility.

---
+++ src/GM.Utility/GM.Utility/EnvironmentUtility.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GM.Utility
+{
+	public static class EnvironmentUtility
+	{
+		/// <summary>
+		/// Gets the bitness of the current Windows operating system. Either 32 or 64.
+		/// </summary>
+		public static int GetWindowsBit()
+		{
+			return Environment.Is64BitOperatingSystem ? 64 : 32;
+		}
+	}
+}
