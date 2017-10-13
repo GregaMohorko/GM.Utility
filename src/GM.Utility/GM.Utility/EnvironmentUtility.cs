33c933c2f7993cd277fadc8630eb3779e67b0b68
Grega Mohorko <grega@mohorko.info>
2017-10-13 10:16:15 +0200

    Added support for bitness and architecture of environment.

--- src/GM.Utility/GM.Utility/EnvironmentUtility.cs
+++ src/GM.Utility/GM.Utility/EnvironmentUtility.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,10 +10,46 @@ namespace GM.Utility
 	{
 		/// <summary>
 		/// Gets the bitness of the current Windows operating system. Either 32 or 64.
+		/// <para>
+		/// This method is deprecated, please use GetOperatingSystemBitness instead.
+		/// </para>
 		/// </summary>
+		[Obsolete("Method GetWindowsBit is deprecated, please use GetOperatingSystemBitness instead.",false)]
 		public static int GetWindowsBit()
 		{
+			return GetOperatingSystemBitness();
+		}
+		
+		/// <summary>
+		/// Gets the bitness of the current operating system. Either 32 or 64.
+		/// </summary>
+		public static int GetOperatingSystemBitness()
+		{
 			return Environment.Is64BitOperatingSystem ? 64 : 32;
+		}
+
+		/// <summary>
+		/// Gets the architecture of the current operating system. Either "x64" or "x86".
+		/// </summary>
+		public static string GetOperatingSystemArchitecture()
+		{
+			return Environment.Is64BitOperatingSystem ? "x64" : "x86";
+		}
+
+		/// <summary>
+		/// Gets the bitness of the current process. Either 32 or 64.
+		/// </summary>
+		public static int GetProcessBitness()
+		{
+			return Environment.Is64BitProcess ? 64 : 32;
+		}
+
+		/// <summary>
+		/// Gets the architecture of the current process. Either "x64" or "x86".
+		/// </summary>
+		public static string GetProcessArchitecture()
+		{
+			return Environment.Is64BitProcess ? "x64" : "x86";
 		}
 	}
 }
