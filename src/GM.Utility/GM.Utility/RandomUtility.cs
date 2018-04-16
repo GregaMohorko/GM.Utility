70160708cde3f62c76c9a732c5fd28630a03e70b
GREGA-AW\grega <grega@mohorko.info>
2018-04-16 17:07:48 +0200

    RandomUtility: renamed RandomInts to NextIntegers.

--- src/GM.Utility/GM.Utility/RandomUtility.cs
+++ src/GM.Utility/GM.Utility/RandomUtility.cs
@@ -36,27 +36,15 @@ namespace GM.Utility
 	/// Utilities for <see cref="Random"/>.
 	/// </summary>
     public static class RandomUtility
-    {
+	{
 		/// <summary>
-		/// Creates and returns an array of the specified sizez containing random values within the [low, high] interval.
-		/// </summary>
-		/// <param name="minValue">The inclusive lower bound of the random numbers.</param>
-		/// <param name="maxValue">The exclusive upper bound of the random numbers. maxValue must be greater than or equal to minValue.</param>
-		/// <param name="size">The size of the array to create.</param>
-		public static int[] RandomInts(int minValue, int maxValue, int size)
-		{
-			var rand = new Random();
-			return RandomInts(rand, minValue, maxValue, size);
-		}
-
-		/// <summary>
-		/// Creates and returns an array of the specified sizez containing random values within the [low, high] interval.
+		/// Creates and returns an array of the specified size containing random integer values within the [low, high] interval.
 		/// </summary>
 		/// <param name="rand">The <see cref="Random"/> object to use to create random values.</param>
 		/// <param name="minValue">The inclusive lower bound of the random numbers.</param>
 		/// <param name="maxValue">The exclusive upper bound of the random numbers. maxValue must be greater than or equal to minValue.</param>
 		/// <param name="size">The size of the array to create.</param>
-		public static int[] RandomInts(this Random rand, int minValue, int maxValue, int size)
+		public static int[] NextIntegers(this Random rand, int minValue, int maxValue, int size)
 		{
 			if(rand == null) {
 				throw new ArgumentNullException(nameof(rand));
@@ -67,6 +55,49 @@ namespace GM.Utility
 				result[i] = rand.Next(minValue, maxValue);
 			}
 			return result;
+		}
+
+		/// <summary>
+		/// Creates and returns an array of the specified size containing random integer values within the [low, high] interval.
+		/// </summary>
+		/// <param name="minValue">The inclusive lower bound of the random numbers.</param>
+		/// <param name="maxValue">The exclusive upper bound of the random numbers. maxValue must be greater than or equal to minValue.</param>
+		/// <param name="size">The size of the array to create.</param>
+		public static int[] RandomIntegers(int minValue, int maxValue, int size)
+		{
+			var rand = new Random();
+			return NextIntegers(rand, minValue, maxValue, size);
+		}
+
+		/// <summary>
+		/// Creates and returns an array of the specified size containing random values within the [low, high] interval.
+		/// <para>This method is deprecated, please use <see cref="RandomIntegers(int, int, int)"/> instead.</para>
+		/// </summary>
+		/// <param name="minValue">The inclusive lower bound of the random numbers.</param>
+		/// <param name="maxValue">The exclusive upper bound of the random numbers. maxValue must be greater than or equal to minValue.</param>
+		/// <param name="size">The size of the array to create.</param>
+		[Obsolete("Method RandomInts is deprecated, please use RandomIntegers instead.",true)]
+		public static int[] RandomInts(int minValue, int maxValue, int size)
+		{
+			// FIXME obsolete 2018-4-16
+			return RandomIntegers(minValue, maxValue, size);
+		}
+
+		/// <summary>
+		/// Creates and returns an array of the specified size containing random values within the [low, high] interval.
+		/// <para>
+		/// This method is deprecated, please use <see cref="NextIntegers(Random, int, int, int)"/> instead.
+		/// </para>
+		/// </summary>
+		/// <param name="rand">The <see cref="Random"/> object to use to create random values.</param>
+		/// <param name="minValue">The inclusive lower bound of the random numbers.</param>
+		/// <param name="maxValue">The exclusive upper bound of the random numbers. maxValue must be greater than or equal to minValue.</param>
+		/// <param name="size">The size of the array to create.</param>
+		[Obsolete("Method RandomInts is deprecated, please use NextIntegers instead.",true)]
+		public static int[] RandomInts(this Random rand, int minValue, int maxValue, int size)
+		{
+			// FIXME obsolete 2018-4-16
+			return NextIntegers(rand, minValue, maxValue, size);
 		}
 	}
 }
