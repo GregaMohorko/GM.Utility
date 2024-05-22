88d58fd8fa62c347749a2abf37279483cb2ba338
Gregor Mohorko <grega@mohorko.info>
2024-05-22 13:12:19 +0200

    feat: Add TimeSpanUtility

---
+++ src/GM.Utility/GM.Utility/TimeSpanUtility.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GM.Utility;
+/// <summary>
+/// Utilities for <see cref="TimeSpan"/>.
+/// </summary>
+public static class TimeSpanUtility
+{
+	/// <summary>
+	/// Returns the maximum value of the two provided values.
+	/// </summary>
+	/// <param name="val1">The first value.</param>
+	/// <param name="val2">The second value.</param>
+	public static TimeSpan Max(TimeSpan val1, TimeSpan val2)
+	{
+		return val1 > val2 ? val1 : val2;
+	}
+}
